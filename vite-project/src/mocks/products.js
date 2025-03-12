96932405fa4246671a73f64d2d180405c7de9e9c
Uladzisalu <vladshokoladkin@gmail.com>
2025-03-12 17:54:41 +0200

    refactored

---
+++ vite-project/src/mocks/products.js
@@ -0,0 +1,57 @@
+import BurgerA from "../assets/BurgerA.png";
+import BurgerB from "../assets/BurgerB.png";
+import BurgerC from "../assets/BurgerC.png";
+import BurgerD from "../assets/BurgerD.png";
+import BurgerE from "../assets/BurgerE.png";
+import BurgerF from "../assets/BurgerF.png";
+
+export const products = [
+  {
+    id: 1,
+    name: "Burger Dreams",
+    price: 9.2,
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    image: BurgerA,
+  },
+  {
+    id: 2,
+    name: "Burger Waldo",
+    price: 10.0,
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    image: BurgerB,
+  },
+  {
+    id: 3,
+    name: "Burger Cali",
+    price: 8.0,
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    image: BurgerC,
+  },
+  {
+    id: 4,
+    name: "Burger Bacon Buddy",
+    price: 9.99,
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    image: BurgerD,
+  },
+  {
+    id: 5,
+    name: "Burger Spicy",
+    price: 9.2,
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    image: BurgerE,
+  },
+  {
+    id: 6,
+    name: "Burger Classic",
+    price: 8,
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    image: BurgerF,
+  },
+];
